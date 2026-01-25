76683bd4c56a3b582fc8ddedf86c0c56ae6a0a06
krishna singh <0.krishna1120@gmail.com>
2026-01-25 17:41:44 +0530

    feat: add script.js with initial function implementations and remove index.html and style.css

---
+++ js-basics/script.js
@@ -0,0 +1,65 @@
+// q1
+
+// function getOutput(data) {
+//     const result = {};
+//     for(let category in data) {
+//        const len = data[category].length;
+//        for(let i=0; i<len; i++) {
+//            if(!result[category]) {
+//                result[category] = 0;
+//            }   
+//            result[category] += data[category][i];
+//        }
+//     }
+//     return result;
+// }
+// const input = { food: [10, 20, 30], travel: [5, 15], bills: [40, 60] };
+// const ans = getOutput(input);
+// console.log(ans)
+
+// q2
+// const input = ["apple", "banana", "apple", "orange", "banana", "apple"];
+
+// function getFrequency(arr) {
+//     const frequency = {};
+//     for (let i = 0; i < arr.length; i++) {
+//         const item = arr[i];
+//         if (frequency[item]) {
+//             frequency[item] += 1;
+//         } else {
+//             frequency[item] = 1;
+//         }
+//     }
+//     return frequency;   
+// }
+// const ans = getFrequency(input);
+// console.log(ans);
+
+// q3
+// const input = { a: "x", b: "y", c: "z" };
+// function swapKeyValue(obj) {
+//     const swapper = {}
+//     for(let key in obj){
+//         const val = obj[key];
+//         swapper[val] = key;
+//     }
+//     return swapper
+// }
+// const ans = swapKeyValue(input);
+// console.log(ans);
+
+// q4
+// const input = { a: 10, b: 50, c: 20 }
+// function getMax(input){
+//         let Largest = -Infinity;
+//         for(let key in input){
+//             if(input[key] > Largest){
+//                 Largest = input[key];
+//             }
+//         }
+//         return Largest;
+//     }
+// const ans = getMax(input);
+// console.log(ans);
+
+// q5
